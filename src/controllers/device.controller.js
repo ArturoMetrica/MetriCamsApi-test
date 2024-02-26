facde0b8c604f1266f3b806a13d1799ee1c56a2b
Jose Rdz <joseluisrdzs@live.com>
2024-02-26 10:28:31 -0600

    fix status & auditlog

--- src/controllers/device.controller.js
+++ src/controllers/device.controller.js
@@ -4,104 +4,6 @@ const DBService = require('../services/database');
 const { errorLogs } = new DBService();
 const handleResponseUtil = require('../utils/handleResponse.util');
 
-class DeviceController {
-	addDevice = async () => {
-		try {
-			const { deviceData, cameras, streamaxFleetId } = req.device;
-
-			switch (deviceData.deviceType) {
-				case 'GO':
-					await deviceService.cudDevice(req.sessionid.sessionid, req.device)
-					break;
-				case 'MDVR':
-					const { success, message } = await FTService.addListDevices('', [{
-						channels: cameras.map(camera => camera.chl).join(','),
-						uniqueId: deviceData.deviceSerial,
-					}]);
-
-					if (!success)
-						return handleResponseUtil(res, 500, false, message || 'Streamax error', null);
-
-					await FTService.changeDeviceFleet(streamaxFleetId, deviceData.deviceSerial);
-
-					await deviceService.cudDevice(req.sessionid.sessionid, req.device)
-					break;
-				case 'LYTX':
-					await deviceService.cudDevice(req.sessionid.sessionid, req.device)
-					break;
-				default:
-					break;
-			}
-		} catch (error) {
-			await errorLogs('API', error, '/api/device');
-
-			handleResponseUtil(res, 500, false, error.message || error, null);
-		}
-	}
-
-	updateDevice = async () => {
-		try {
-			const { deviceData, cameras, streamaxFleetId } = req.device;
-
-			switch (deviceData.deviceType) {
-				case 'GO':
-					await deviceService.cudDevice(req.sessionid.sessionid, req.device)
-					break;
-				case 'MDVR':
-					await FTService.deleteDevice('', deviceData.deviceSerial);
-
-					const { success, message } = await FTService.addListDevices('', [{
-						channels: cameras.map(camera => camera.chl).join(','),
-						uniqueId: deviceData.deviceSerial,
-					}]);
-
-					if (!success)
-						return handleResponseUtil(res, 500, false, message || 'Streamax error', null);
-
-					await FTService.changeDeviceFleet(streamaxFleetId, deviceData.deviceSerial);
-
-					await deviceService.cudDevice(req.sessionid.sessionid, req.device)
-					break;
-				case 'LYTX':
-					await deviceService.cudDevice(req.sessionid.sessionid, req.device)
-					break;
-				default:
-					break;
-			}
-		} catch (error) {
-			await errorLogs('API', error, '/api/device');
-
-			handleResponseUtil(res, 500, false, error.message || error, null);
-		}
-	}
-
-	deleteDevice = async () => {
-		try {
-			const { deviceData } = req.device;
-
-			switch (deviceData.deviceType) {
-				case 'GO':
-					await deviceService.cudDevice(req.sessionid.sessionid, req.device)
-					break;
-				case 'MDVR':
-					await FTService.deleteDevice('', deviceData.deviceSerial);
-
-					await deviceService.cudDevice(req.sessionid.sessionid, req.device)
-					break;
-				case 'LYTX':
-					await deviceService.cudDevice(req.sessionid.sessionid, req.device)
-					break;
-				default:
-					break;
-			}
-		} catch (error) {
-			await errorLogs('API', error, '/api/device');
-
-			handleResponseUtil(res, 500, false, error.message || error, null);
-		}
-	}
-}
-
 // module.exports = new DeviceController();
 const addDevice = async (req, res) => {
 	try {
@@ -131,8 +33,9 @@ const addDevice = async (req, res) => {
 			}
 		}
 
-		handleResponseUtil(res, 200, false, 'ok', null);
+		handleResponseUtil(res, 200, true, 'ok', null);
 	} catch (error) {
+		await errorLogs('API', error, '/api/device');
 		handleResponseUtil(res, 500, false, error.message || error, null);
 	}
 }
@@ -167,8 +70,9 @@ const updateDevice = async (req, res) => {
 			}
 		}
 
-		handleResponseUtil(res, 200, false, 'ok', null);
+		handleResponseUtil(res, 200, true, 'ok', null);
 	} catch (error) {
+		await errorLogs('API', error, '/api/device');
 		handleResponseUtil(res, 500, false, error.message || error, null);
 	}
 }
@@ -193,8 +97,9 @@ const deleteDevice = async (req, res) => {
 			}
 		}
 
-		handleResponseUtil(res, 200, false, 'ok', null);
+		handleResponseUtil(res, 200, true, 'ok', null);
 	} catch (error) {
+		await errorLogs('API', error, '/api/device');
 		handleResponseUtil(res, 500, false, error.message || error, null);
 	}
 }
