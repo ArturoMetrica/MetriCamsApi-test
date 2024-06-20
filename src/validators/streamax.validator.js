e44dd1ae5790843cba2023bcd638d9a802c54f8d
smazucaMM <salma.mazuca@metricamovil.com>
2024-06-20 16:53:52 -0600

    Add lack of params to update rule

--- src/validators/streamax.validator.js
+++ src/validators/streamax.validator.js
@@ -48,7 +48,24 @@ class StreamaxValidator {
             isEmail: Joi.boolean().required(),
             secPre: Joi.number().required(),
             secPoe: Joi.number().required(),
-            isActive: Joi.boolean().required()
+            isActive: Joi.boolean().required(),
+            emailList: Joi.array().required(),
+            creationDate: Joi.date().required().default(null),
+            gifRequired: Joi.boolean().required().default(false),
+            videoRequired: Joi.boolean().required().default(false),
+            alarmCategoryId: Joi.number().required().default(2),
+            zoneRestrictionIdEntry: Joi.string().required().default(''),
+            zoneRestrictionNameEntry: Joi.string().required().default(''),
+            zoneRestrictionIdExit: Joi.string().required().default(''),
+            zoneRestrictionNameExit: Joi.string().required().default(''),
+            isZoneRestriction: Joi.boolean().required().default(false),
+            zoneGeofence: Joi.boolean().required().default(false),
+            zoneRuleIdEntry: Joi.string().required().default(''),
+            zoneRuleNameEntry: Joi.string().required().default(''),
+            zoneRuleIdExit: Joi.string().required().default(''),
+            zoneRuleNameExit: Joi.string().required().default(''),
+            cameraType: Joi.string().required().default('DSM'),
+            cameTypeId: Joi.number().required().default(1)
         }).options({ allowUnknown: true, stripUnknown: true });
     }
 
