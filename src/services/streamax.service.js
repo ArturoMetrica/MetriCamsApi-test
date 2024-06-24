4c47b8e8406ba765c00cf600a13921a265fb81e6
smazucaMM <salma.mazuca@metricamovil.com>
2024-06-24 17:52:38 -0600

    Chage type of validation for update params

--- src/services/streamax.service.js
+++ src/services/streamax.service.js
@@ -41,7 +41,7 @@ class StreamaxService {
 
     updateStreamaxRule = async (sessionId, {idRule, idVehicle, idFleet, idAlarm, ruleName, desc, isPublic, isPopup, isEmail, secPre, secsPos, isActive, emailList, creationDate, gifRequired, videoRequired, alarmCategoryId, zoneRestrictionIdEntry, zoneRestrictionNameEntry, zoneRestrictionIdExit, zoneRestrictionNameExit, isZoneRestriction, zoneGeofence, zoneRuleIdEntry, zoneRuleNameEntry, zoneRuleIdExit, zoneRuleNameExit, cameraType, cameraTypeId}) => {
         try {
-            return await query('SELECT * FROM update_streamax_rules_fn($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30) AS QUERY', [
+            return await query('SELECT * FROM update_streamax_rules_fn($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30) AS DATA', [
                 idRule,
                 sessionId,
                 idVehicle,
