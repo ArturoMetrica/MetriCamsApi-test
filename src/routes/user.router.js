61151082c369e065bfba9bfffaf73c2e1699bbac
MMotor <angel.soto@metricamovil.com>
2024-08-13 16:34:06 -0600

    Minimizado de modulos por usuario

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,6 +3,7 @@ const baseRouter = require('express').Router();
 
 const Controller = require('../controllers/user.controller');
 const Middleware = require('../middlewares/user.middleware');
+const tokenMiddleware = require('../middlewares/token.middleware');
 
 router.get('/registeredusers', Middleware.getRegisteredUsers, Controller.getRegisteredUsers);
 router.get('/user/complete-register', Middleware.completeRegister, Controller.completeRegister);
@@ -10,6 +11,9 @@ router.put('/user', Middleware.updateUser, Controller.updateUser);
 router.delete('/user', Middleware.deleteUser, Controller.deleteUser);
 router.post('/user/duration', Middleware.setUserDuration, Controller.setUserDuration);
 
+//Modulos de usuario
+router.get('/user/modules', tokenMiddleware.verify, Middleware.getListModulesUser, Controller.getListModulesUser)
+router.put('/user/modules', tokenMiddleware.verify, Middleware.updateListModulesUser, Controller.updateListModulesUser)
 
 baseRouter.use('/api', router);
 module.exports = baseRouter;
